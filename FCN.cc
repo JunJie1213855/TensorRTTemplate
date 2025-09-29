da88da6a05b9238558f03dfc07a0b6a47c4f1d45
handsome boy <100855117+5p6@users.noreply.github.com>
2025-09-29 16:24:31 +0800

    Update FCN.cc

--- FCN.cc
+++ FCN.cc
@@ -7,12 +7,6 @@ namespace FCN
     {
         cv::Mat imgc = img.clone();
 
-        // Resize to target size (256, 256) as specified in config
-        if (imgc.size() != cv::Size(256, 256))
-        {
-            cv::resize(imgc, imgc, cv::Size(256, 256));
-        }
-
         // Convert BGR to RGB as specified in config (bgr_to_rgb=True)
         cv::Mat rgb_img;
         cv::cvtColor(imgc, rgb_img, cv::COLOR_BGR2RGB);
